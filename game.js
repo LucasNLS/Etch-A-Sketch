63c0a6d38ea5d4ddcbfadb7bfc18b3e60fe809ba
Lucas <62632191+LucasNLS@users.noreply.github.com>
2023-01-05 11:51:24 -0600

    Fixed structure, and added paint, and clear functionality

--- game.js
+++ game.js
@@ -1,6 +1,41 @@
+const gridcontainer = document.querySelector(".grid-container");
+
 function createGrid(amount) {
   let i = 1;
-  while (i <= amount) {}
+  let j = 1;
+  while (j <= amount) {
+    const div = document.createElement("div");
+    div.classList.add("row");
+    while (i <= amount) {
+      const divchild = document.createElement("div");
+      divchild.classList.add("grid");
+      div.appendChild(divchild);
+      i++;
+    }
+    i = 1;
+    gridcontainer.appendChild(div);
+    j++;
+  }
 }
 
-const gridcontainer = document.querySelector("grid-container");
+function changeBackground(e) {
+  this.classList.add("painted");
+  e.stopPropagation();
+}
+
+function clearBackground() {
+  const squares = document.getElementsByClassName("grid");
+  for (let x = 0; x < squares.length; x++) {
+    squares[x].classList.remove("painted");
+  }
+}
+
+createGrid(20);
+
+const squares = document.getElementsByClassName("grid");
+for (let x = 0; x < squares.length; x++) {
+  squares[x].addEventListener("mouseover", changeBackground);
+}
+
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", clearBackground);
